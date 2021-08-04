13342c02def4470e23e674fba7bab206ae9d81c9
Sam Kenney <sam.kenney@louder.com.au>
2021-08-04 23:49:25 +1000

    Adding in some basic user accessible functions

--- sk_notes/__init__.py
+++ sk_notes/__init__.py
@@ -2,9 +2,11 @@
 from .bq_handler import BigQueryOperations
 from .constants import EXAMPLE_NOTE
 from .local_handler import LocalHandler
+from .notes import CreateNote
 
 __all__ = [
     "BigQueryOperations",
+    "CreateNote",
     "EXAMPLE_NOTE",
     "LocalHandler",
 ]
