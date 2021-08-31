75c2cf777626f6de21f85d3c7d99887e87f8a310
Sam Kenney <sam.kenney@louder.com.au>
2021-08-31 23:51:44 +1000

    Removing BigQuery operations classes

--- sk_notes/__init__.py
+++ sk_notes/__init__.py
@@ -1,5 +1,4 @@
 """Expose public classes and methods from module."""
-from .bq_handler import BigQueryOperations
 from .constants import EXAMPLE_NOTE
 from .local_handler import LocalHandler
 from .note_handler import NewNote, CreateNote, DeleteNote, DisplayNote, UpdateNote
@@ -7,7 +6,6 @@ from .notes_functions import Notes
 from .settings import SetUp
 
 __all__ = [
-    "BigQueryOperations",
     "CreateNote",
     "DeleteNote",
     "DisplayNote",
